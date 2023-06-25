5e2b29ce40f5442b7444633ab5cccc6ba9261cbb
bilele123123 <tple06203@gmail.com>
2023-06-24 19:16:47 -0700

    Set up website layout.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './assets/maplestory.otf'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
